d796309fbdac0ae1a5d5ab4cb8b6b90ef92c4aa1
Quanisha Jackson <jquanisha0031@gmail.com>
2021-07-13 20:48:11 -0500

    Fixed spelling errors; added .env

--- config/connection.js
+++ config/connection.js
@@ -9,11 +9,11 @@ if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(
         process.env.DB_NAME,
         process.env.DB_USER,
-        process.env.DB_Password,
+        process.env.DB_PASSWORD,
 
         {
             host: 'localhost',
-            dialect: 'mqsql',
+            dialect: 'mysql',
             port: 3306,
 
         }
