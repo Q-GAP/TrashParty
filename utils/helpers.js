fa843aa18f6bb8ce44481d630dbe76ec861ac8be
Peter Roto <peterdroto@gmail.com>
2021-07-17 14:11:04 -0500

    MVP login Screen Styling

---
+++ utils/helpers.js
@@ -0,0 +1,15 @@
+module.exports = {
+    format_time: (date) => {
+        return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+    },
+    format_date: (date) => {
+        return date.toLocaleDateString();
+    },
+    format_plural: (word, amount) => {
+        if (amount !== 1) {
+            return `${word}s`;
+        }
+
+        return word;
+    }
+}
