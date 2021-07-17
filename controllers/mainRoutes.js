b503d168f43b538bdbe4e94e7fe2f7ae55f78532
Peter Roto <peterdroto@gmail.com>
2021-07-17 14:11:04 -0500

    MVP login Screen Styling

--- controllers/mainRoutes.js
+++ controllers/mainRoutes.js
@@ -6,28 +6,28 @@ const { userTrash, Trash, User } = require('../models');
 //If the user is logged in, redirect to dashboard. If not render the login page.
 
 router.get('/login', (req, res) => {
-    // if (req.session.loggedIn){
-    //     res.redirect('/dashboard');
-    // }else{
     res.render('login');
     return;
-}
 });
 
 
 // TODO: FINISH ROUTE
 router.get('/', (req, res) => {
-    if (loggedIn) {
-        User.findall({
-            attributes: ['id', 'username', 'lastPackOpened', '']
 
-        })
+    res.render('splash')
+        // if (loggedIn) {
+        //     User.findall({
+        //         attributes: ['id', 'username', 'lastPackOpened', '']
+
+    //     })
 
 
-    } else
+    // } else
 
-        User.findall({
-        attributes: ['id', 'lastOpened', 'username']
-    })
+    //     User.findall({
+    //     attributes: ['id', 'lastOpened', 'username']
+    // })
 
 })
+
+module.exports = router
