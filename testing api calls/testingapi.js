7b2da29117d21d06e035699888d93c1983352b13
Quanisha Jackson <jquanisha0031@gmail.com>
2021-07-17 12:13:12 -0500

    API data works

--- testing api calls/testingapi.js
+++ testing api calls/testingapi.js
@@ -1,37 +1,25 @@
 let APIKEY = "ur8sKa88o5YJg8iulM0iYStuzZAuOiQC";
-// you will need to get your own API KEY
-// https://developers.giphy.com/dashboard/
-document.addEventListener("DOMContentLoaded", init);
 
-function init() {
-    document.getElementById("btnSearch").addEventListener("click", ev => {
-        ev.preventDefault(); //to stop the page reload
-        let url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=25&q=`;
-        let str = document.getElementById("search").value.trim();
-        url = url.concat(str);
-        console.log(url);
-        fetch(url)
-            .then(response => response.json())
-            .then(content => {
-                //  data, pagination, meta
-                console.log(content.data);
-                console.log("META", content.meta);
-                let fig = document.createElement("figure");
-                let img = document.createElement("img");
-                let fc = document.createElement("figcaption");
-                img.src = content.data[0].images.downsized.url;
-                img.alt = content.data[0].title;
-                fc.textContent = content.data[0].title;
-                fig.appendChild(img);
-                fig.appendChild(fc);
-                let out = document.querySelector(".out");
-                out.insertAdjacentElement("afterbegin", fig);
-                document.querySelector("#search").value = "";
-            })
-            .catch(err => {
-                console.error(err);
+function fetchData() {
+    console.log("START FETCH");
+    fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${APIKEY}&limit=25"`).then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data.data);
+            const gifList = data.data.map(gif => {
+
+                return { name: gif.title, image: gif.embed_url }
+
             });
-    });
+            console.log(gifList);
+            document
+                .querySelector("#api")
+                .insertAdjacentHTML("afterbegin", "<h1>Hi</h1>");
+        }).catch(errror => {
+            console.log(errror)
+        });
+
 }
 
-// let url = `https://api.giphy.com/v1/gifs/random`
+fetchData();
