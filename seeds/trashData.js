9cc183429fcf4988ea0230bab09d576e41991e92
aik <anthonykrueger0@gmail.com>
2021-07-17 22:42:11 -0500

    make images work right

--- seeds/trashData.js
+++ seeds/trashData.js
@@ -3,33 +3,33 @@ const { Trash } = require('../models')
 const trashData = [
     {
         name: "Test Trash #1",
-        image: "https://giphy.com/embed/3o7527pa7qs9kCG78A",
+        image: "https://media4.giphy.com/media/WYEWpk4lRPDq0/200.gif?cid=c367375du9p9czh2cp9p5o0340dlj402cwhdq2ux60fljt87&rid=200.gif&ct=g",
         rarity: 3,
-        category: "Dog"
+        category: "Cat"
     },
     {
         name: "Test Trash #2",
-        image: "https://giphy.com/embed/4Zo41lhzKt6iZ8xff9",
+        image: "https://media3.giphy.com/media/13CoXDiaCcCoyk/200.gif?cid=c367375du9p9czh2cp9p5o0340dlj402cwhdq2ux60fljt87&rid=200.gif&ct=g",
         rarity: 1,
-        category: "Dog"
+        category: "Cat"
     },
     {
         name: "Test Trash #3",
-        image: "https://giphy.com/embed/WLbtNNR5TKJBS",
+        image: "https://media2.giphy.com/media/ToMjGppLes0ENI5osCc/200.gif?cid=c367375du9p9czh2cp9p5o0340dlj402cwhdq2ux60fljt87&rid=200.gif&ct=g",
         rarity: 3,
-        category: "Dog"
+        category: "Cat"
     },
     {
         name: "Test Trash #4",
-        image: "https://giphy.com/embed/xThtadSLoInlcD1UmA",
+        image: "https://media1.giphy.com/media/cuPm4p4pClZVC/200.gif?cid=c367375du9p9czh2cp9p5o0340dlj402cwhdq2ux60fljt87&rid=200.gif&ct=g",
         rarity: 2,
-        category: "Dog"
+        category: "Cat"
     },
     {
         name: "Test Trash #5",
-        image: "https://giphy.com/embed/QvBoMEcQ7DQXK",
+        image: "https://media0.giphy.com/media/H2GT0TQBAlbuo/200.gif?cid=c367375du9p9czh2cp9p5o0340dlj402cwhdq2ux60fljt87&rid=200.gif&ct=g",
         rarity: 1,
-        category: "Dog"
+        category: "Cat"
     }
 ]
 
