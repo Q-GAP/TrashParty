7caa279b426c9cee910b03271eb03413b5eaf878
Peter Roto <peterdroto@gmail.com>
2021-07-19 22:14:27 -0500

    working on main routes, pushing so @GPLOTKE can help

--- controllers/mainRoutes.js
+++ controllers/mainRoutes.js
@@ -1,27 +1,12 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { userTrash, Trash, User } = require('../models');
+const { UserTrash, Trash, User } = require('../models');
 
 
-//If the user is logged in, redirect to dashboard. If not render the login page.
-
-router.get('/login', (req, res) => {
-    res.render('login');
-    return;
-});
-router.get('/signup', (req, res) => {
-    res.render('login');
-    return;
-});
-
-// TODO: FINISH ROUTE
-router.get('/', (req, res) => {
+router.get('/', async(req, res) => {
     if (req.session.loggedIn) {
-        res.render('dashboard', { username: req.session.username })
-    } else {
         User.findAll({
-            attributes: ['id', 'username', 'lastOpened']
-
+            attributes: ['id', 'username', 'lastOpened'],
         }).then((userData) => {
             if (!userData) {
                 res.status(404).json({ message: "No one's at the TrashPARTY YET!! \n or Your data's corrupt. . ." })
@@ -29,42 +14,70 @@ router.get('/', (req, res) => {
             }
 
             const users = userData.map(user => user.get({ plain: true }));
-
-            res.render('splash', { users, loggedIn: !req.session.loggedIn });
-
+            console.log('\n \n users:' + JSON.stringify(users) + '\n \n');
+            res.render('dashboard');
         })
+    } else {
+        res.render('splash')
     }
 })
 
+
+//If the user is logged in, redirect to dashboard. If not render the login page.
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/dashboard')
+    } else {
+        res.render('login');
+        return;
+    }
+});
+router.get('/signup', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/dashboard')
+    }
+    res.render('login');
+    return;
+});
+
 router.get('/dashboard', (req, res) => {
-    // if (req.session.loggedIn) {
-    User.findAll({
+    if (req.session.loggedIn) {
+        User.findAll({
             where: {
                 // use the ID from the session
-                user_id: req.session.id
+                username: req.session.username
             },
             attributes: [
-                'id',
-                'username',
+                "id",
+                "username",
+                "email",
                 'lastOpened'
             ],
             include: [{
-                model: userTrash,
-                attributes: ['id', 'inLandfill', 'trashId']
-                    // }
-                    // ,{
-                    //     include: [{
-                    //         model: Trash,
-                    //         attributes: ['id', 'name', 'image', 'category', 'rarity']
-                    //     }]
+                model: UserTrash,
+                include: [{
+                    model: Trash
+                }]
             }]
 
+
+            // attributes: ['id', 'name', 'image', 'category', 'rarity'],
+
         }).then(dashData => {
-            const dashinfo = dashData.map(item => item.get({ plain: true }))
-            res.render('dashboard', dashinfo)
+            console.log('\n \n dashData: \n' + dashData + '\n \n')
+            const dashInfo = dashData.map(item => item.get({ plain: true }))
+            const userInfo = dashInfo[0]
+            console.log('\n \n userInfo: \n' + JSON.stringify(dashInfo[0]) + '\n \n')
+            res.render('dashboard', { userInfo, loggedIn: req.session.loggedIn })
         })
-        // }
+    } else {
+        res.status(200).redirect('/login')
+    }
 })
 
+router.get('/splash', (req, res) => {
+    res.render('splash')
+})
 
 module.exports = router
