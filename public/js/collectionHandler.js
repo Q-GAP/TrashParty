65a26884895daa30398bbc15cd3d7c8bffb21adc
Peter Roto <peterdroto@gmail.com>
2021-07-20 23:28:38 -0500

    Debugging merge

--- public/js/collectionHandler.js
+++ public/js/collectionHandler.js
@@ -1,15 +1,13 @@
-const landfillAddHandler = async (event) => {
+const landfillAddHandler = async(event) => {
     const trashId = event.target.dataset.trashid
     const response = await fetch(`/api/usertrash/landfill/${trashId}`, {
         method: "GET",
         headers: { 'Content-Type': 'application/json' }
     })
-    if(response.ok) {
+    if (response.ok) {
         document.location.reload()
     }
 
-<<<<<<< HEAD
-=======
 }
 
 const landfillButtons = document.querySelectorAll(".landfillBtn")
@@ -17,7 +15,6 @@ landfillButtons.forEach((button) => {
     button.addEventListener("click", landfillAddHandler)
 })
 
->>>>>>> a864258d21bc6766713bb2026f815927042abd19
 $(window).onload = function() {
     $('.window').css('background-image', "url('../images/landfillwallpaper.jpg')");
 }
