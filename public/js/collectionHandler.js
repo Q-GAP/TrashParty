0c0a2a327137bc064eb9e229988f9e0faf975b8e
aik <anthonykrueger0@gmail.com>
2021-07-20 21:04:52 -0500

    updated collection page, added landfill button, landfill in the works

--- public/js/collectionHandler.js
+++ public/js/collectionHandler.js
@@ -1,2 +1,17 @@
-const trashImages = $(".trashImage")
+const landfillAddHandler = async (event) => {
+    const trashId = event.target.dataset.trashid
+    const response = await fetch(`/api/usertrash/landfill/${trashId}`, {
+        method: "GET",
+        headers: { 'Content-Type': 'application/json' }
+    })
+    if(response.ok) {
+        document.location.reload()
+    }
+
+}
+
+const landfillButtons = document.querySelectorAll(".landfillBtn")
+landfillButtons.forEach((button) => {
+    button.addEventListener("click", landfillAddHandler)
+})
 
