258e9fe040996d86d0d2081a0710bbfaeb7ffe5d
aik <anthonykrueger0@gmail.com>
2021-07-21 23:15:57 -0500

    landfillUpdate function added

--- controllers/api/landfill-routes.js
+++ controllers/api/landfill-routes.js
@@ -1,8 +1,26 @@
 const router = require('express').Router();
 const { Trash, User, UserTrash } = require('../../models')
 
+const landfillUpdate = async () => {
+    const landfillList = await UserTrash.findAll({where: {inLandfill: true}, order: [
+        ['updatedAt', 'DESC']
+    ],})
+    landfillList.forEach( async (trash) => {
+        if(trash.updatedAt - Date.now() >= 86400000) {
+            await trash.destroy()
+        }
+    })
+    if(landfillList.length > 100) {
+        while(landfillList > 100) {
+            landfillList[(landfillList.length - 1)].destroy()
+        }
+    }
+    return;
+}
+
 router.get('/:id', async (req, res) => {
     try {
+        await landfillUpdate();
         const movingTrash = await UserTrash.findByPk(req.params.id)
         if(movingTrash.userId == req.session.userId) {
             movingTrash.inLandfill = true;
@@ -20,6 +38,7 @@ router.get('/:id', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
     try {
+        await landfillUpdate();
         const movingTrash = await UserTrash.findByPk(req.params.id)
         if(req.session.userId && movingTrash.inLandfill == true) {
             movingTrash.inLandfill = false;
