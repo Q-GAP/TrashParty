7e7147eb0f1d5b90a55524d3caffe4f7e2748af0
Quanisha Jackson <jquanisha0031@gmail.com>
2021-07-21 22:30:05 -0500

    Added stop GIF option; styled CSS

--- public/js/index.js
+++ public/js/index.js
@@ -18,9 +18,23 @@ const logout = async() => {
 if (document.location.pathname === "/collection" || document.location.pathname === "/landfill" || document.location.pathname === "/collection/" || document.location.pathname === "/landfill/") {
     $('.window').css('background-image', "url('/images/landfillwallpaper.jpeg')");
     $('.window').css('background-size', "unset");
+    $('#stopGIF').hide();
+    $('#startGIF').hide();
 } else {
     $('window').css('background-image', "url('/images/tpbg.gif')")
     $('.window').css('background-size', "cover");
+    $('#stopGIF').show();
 }
+
+$('#stopGIF').on('click', () => {
+    $('.window').css('background-image', 'unset')
+    $('#stopGIF').hide();
+    $('#startGIF').show();
+})
+$('#startGIF').on('click', () => {
+    $('.window').css('background-image', "url('https://bafybeiebr2pz4p3itoh7skbdjpzaifrqdujzl2bsibfsi5j3wsra7m7pfq.ipfs.dweb.link/')")
+    $('#startGIF').hide();
+    $('#stopGIF').show();
+})
 
 $('#logout').on('click', logout);
